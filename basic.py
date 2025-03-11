13f8a6f0f3c1d59ac38e947dccad0297feba969f
Mayank-1-0 <mayankrai368@gmail.com>
2025-03-11 22:05:09 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,1 +1,17 @@
+##output
 print('hello world')
+
+## variables
+
+name="Mayank Rai"
+print(name)
+
+age=22
+print(age)
+
+isMale=True
+print(isMale)
+
+##Input
+Name=input("Identify yourself")
+print(Name)
